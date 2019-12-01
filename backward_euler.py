48a19d9a51dcc791e191400a0c4fb291c26b4421
Alexander Fisher <alexandermfisher@eduroam-104-158.nomadic.bris.ac.uk>
2019-12-01 12:23:01 +0000

    added file

---
+++ backward_euler.py
@@ -0,0 +1,24 @@
+# simple forward Euler solver for the 1D heat equation
+#   u_t = kappa u_xx  0<x<L, 0<t<T
+# with zero-temperature boundary conditions
+#   u=0 at x=0,L, t>0
+# and prescribed initial temperature
+#   u=u_I(x) 0<=x<=L,t=0
+
+import numpy as np
+import pylab as pl
+from math import pi
+
+# set problem parameters/functions
+kappa = 1.0   # diffusion constant
+L=1.0         # length of spatial domain
+T=0.5        # total time to solve for
+def u_I(x):
+    # initial temperature distribution
+    y = np.sin(pi*x/L)
+    return y
+
+def u_exact(x,t):
+    # the exact solution
+    y = np.exp(-kappa*(pi**2/L**2)*t)*np.sin(pi*x/L)
+    return y
