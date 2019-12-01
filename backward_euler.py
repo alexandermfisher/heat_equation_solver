08001c8e7da4a6e1f1c93dde1127542e32244e6d
af17918 <af17918@bristol.ac.uk>
2019-12-01 14:04:33 +0000

    altered forwrd_euler code to implement backward euler method

--- backward_euler.py
+++ backward_euler.py
@@ -5,9 +5,12 @@
 # and prescribed initial temperature
 #   u=u_I(x) 0<=x<=L,t=0
 
+
 import numpy as np
 import pylab as pl
 from math import pi
+from scipy.sparse import diags
+from scipy.sparse.linalg import spsolve
 
 # set problem parameters/functions
 kappa = 1.0   # diffusion constant
@@ -22,3 +25,98 @@ def u_exact(x,t):
     # the exact solution
     y = np.exp(-kappa*(pi**2/L**2)*t)*np.sin(pi*x/L)
     return y
+
+
+# set numerical parameters
+mx = 1000     # number of gridpoints in space
+mt = 2000   # number of gridpoints in time
+
+x = np.linspace(0, L, mx+1)     # mesh points in space
+t = np.linspace(0, T, mt+1)     # mesh points in time
+deltax = x[1] - x[0]            # gridspacing in x
+deltat = t[1] - t[0]            # gridspacing in t
+lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
+print("deltax=",deltax)
+print("deltat=",deltat)
+print("lambda=",lmbda)
+
+
+
+# Data Structure for linear system using sparse matrices:
+
+main_diag  = np.zeros(mx+1)
+lower_diag = np.zeros(mx)
+upper_diag = np.zeros(mx)
+solution_b = np.zeros(mx+1)
+
+# Insert main diaginals
+main_diag[:] = 1 + 2*lmbda
+lower_diag[:] = -lmbda
+upper_diag[:] = -lmbda
+
+# Insert boundary conditions
+main_diag[0] = 0; main_diag[mx] = 0
+
+
+A = diags(diagonals=[main_diag, lower_diag, upper_diag],offsets=[0, -1, 1], shape=(mx+1, mx+1),format="csr")
+#print(A.todense())  # Check that A is correct
+
+
+
+
+# set up the solution variables
+u_j = np.zeros(x.size)        # u at current time step
+u_jn = np.zeros(x.size)      # u at next time step
+
+
+
+# Set initial condition
+for i in range(0, mx+1):
+    u_j[i] = u_I(x[i])
+
+
+for n in range(0, mt):
+    b = u_j
+    b[0] = b[-1] = 0
+    u_jn[:] = spsolve(A, b)
+
+    # Update u_j
+    u_j[:] = u_jn[:]
+
+
+
+# plot the final result and exact solution
+pl.plot(x,u_j,'ro',label='num')
+xx = np.linspace(0,L,250)
+pl.plot(xx,u_exact(xx,T),'b-',label='exact')
+pl.xlabel('x')
+pl.ylabel('u(x,0.5)')
+pl.legend(loc='upper right')
+pl.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
